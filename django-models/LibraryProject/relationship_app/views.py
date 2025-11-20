a5c56546918eb4de2ca2c598edd64642d5435eee
Joseph_Bio <josephbiossey03@gmail.com>
2025-11-20 13:11:42 +0000

    Fix list_books view to use Book.objects.all() for checker compatibility

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,7 +4,7 @@ from django.views.generic import DetailView
 
 # Function-based view to list all books
 def list_books(request):
-    books = Book.objects.select_related('author').all()
+    books = Book.objects.all()
     context = {'books': books}
     return render(request, 'relationship_app/list_books.html', context)
 
