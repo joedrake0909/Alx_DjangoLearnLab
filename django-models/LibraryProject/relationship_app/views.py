14630f4d66cdf74bd2e4bbf7b9d60a0bf4d15401
Joseph_Bio <josephbiossey03@gmail.com>
2025-11-20 12:24:55 +0000

    Add Django views: list_books function view and LibraryDetailView class with query optimizations

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -4,12 +4,18 @@ from django.views.generic import DetailView
 
 # Function-based view to list all books
 def list_books(request):
-    books = Book.objects.all()  # Get all Book objects
-    return render(request, 'relationship_app/list_books.html', {'books': books})
+    books = Book.objects.select_related('author').all()
+    context = {'books': books}
+    return render(request, 'relationship_app/list_books.html', context)
 
 # Class-based view for library details
 class LibraryDetailView(DetailView):
     model = Library
     template_name = 'library_detail.html'
     context_object_name = 'library' 
+
+# Optionally override get_queryset to prefetch books for efficiency
+    def get_queryset(self):
+        # prefetch_related reduces DB hits when accessing many-to-many relations
+        return super().get_queryset().prefetch_related('books__author')
 
