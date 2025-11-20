726032be0223e482737a9419000047fe23db5f43
Joseph_Bio <josephbiossey03@gmail.com>
2025-11-20 12:44:33 +0000

    Register models in Django admin with customized display and search options

--- django-models/LibraryProject/relationship_app/admin.py
+++ django-models/LibraryProject/relationship_app/admin.py
@@ -7,3 +7,17 @@ admin.site.register(Library)
 admin.site.register(Librarian)
 
 # Register your models here.
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    search_fields = ('title', 'author__name')
+
+@admin.register(Library)
+class LibraryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    filter_horizontal = ('books',)  # nicer widget when many books
