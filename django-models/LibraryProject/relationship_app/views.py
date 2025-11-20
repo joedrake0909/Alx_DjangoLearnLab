103423aeab78c730a200bfc2969759b664f8a6e9
Joseph_Bio <josephbiossey03@gmail.com>
2025-11-20 13:23:56 +0000

    Fix syntax error and template path for LibraryDetailView

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Book, Library
+from .models import Library, Book  # REMOVED EXTRA COMMA
 from django.views.generic import DetailView
 
 # Function-based view to list all books
@@ -11,7 +11,5 @@ def list_books(request):
 # Class-based view for library details
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'
-    context_object_name = 'library' 
-
-
+    template_name = 'relationship_app/library_detail.html'  #  ADDED 'relationship_app/'
+    context_object_name = 'library'
