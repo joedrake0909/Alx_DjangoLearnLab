6544631abe80f478d94660ac5a79182df587bee9
Joseph_Bio <josephbiossey03@gmail.com>
2025-11-20 13:57:52 +0000

    Resolve merge conflict in urls.py

--- django-models/LibraryProject/LibraryProject/urls.py
+++ django-models/LibraryProject/LibraryProject/urls.py
@@ -3,10 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-<<<<<<< HEAD
-    path('', include('relationship_app.urls', namespace="relationship_app")),  # ← CORRECT
+    path('', include('relationship_app.urls', namespace="relationship_app")),
 ]
-=======
-    path('', include('relationship_app.urls'), namespace="relationship_app"),  # Include app URLs
-]
->>>>>>> 334fe33c6989c2809a74e13e3f924dc8223f1f45
