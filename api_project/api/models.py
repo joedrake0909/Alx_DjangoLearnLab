a8f0b44dbef2761a377063b4036a8b0a39a17310
Joseph_Bio <josephbiossey03@gmail.com>
2025-11-30 18:46:20 +0000

    Fix the syntax error in the seventh line

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -4,9 +4,8 @@ class Book(models.Model):
     title = models.CharField(max_length=200)
     author = models.Charfield(max_length=100)
     published_date = models.DateField(null=True, blank=True)
-    created at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"{self.title} by {self.author}"
-    
     
