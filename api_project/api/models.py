15cfa7ce92a04244d633920635111c4ac7a517b2
Joseph_Bio <josephbiossey03@gmail.com>
2025-11-30 18:48:23 +0000

    Fix the error in line 5

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.Charfield(max_length=100)
+    author = models.CharField(max_length=100)
     published_date = models.DateField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
