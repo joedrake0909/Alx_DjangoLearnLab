234bd86bd211268d40b3fbdb97c36089d3365a66
Joseph_Bio <josephbiossey03@gmail.com>
2025-11-30 18:00:56 +0000

    Defined a models class called Book

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.Charfield(max_length=100)
+    published_date = models.DateField(null=True, blank=True)
+    created at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
+    
+    
