c6b6b047e3add3afab088fb782ad06c4a2f9684f
Joseph_Bio <josephbiossey03@gmail.com>
2025-12-01 13:36:49 +0000

    fix syntax error

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import BookList, BookViewSet
 from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter
+router = DefaultRouter()
 router.register(r'books_all', BookViewSet, basename='book_all')
 
 
