8c27789e736b2df275adcc146ba7de3761ff8ae6
Joseph_Bio <josephbiossey03@gmail.com>
2025-12-01 15:17:28 +0000

    Added the import viewsets

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
