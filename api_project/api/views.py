6e298cf95ba78928bd01d979fc4c97a88b29e49f
Joseph_Bio <josephbiossey03@gmail.com>
2025-12-02 03:59:30 +0000

    Added a comment

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -9,6 +9,8 @@ class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+# Permissions restrict API access to authenticated users only.
+
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
