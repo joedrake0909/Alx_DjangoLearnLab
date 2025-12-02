3b1ef59d4fcd015d5c44ccc7635ad2c76b4f4f83
Joseph_Bio <josephbiossey03@gmail.com>
2025-12-02 03:57:00 +0000

    Apply Permissions to Your Views

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 
 class BookList(generics.ListAPIView):
@@ -12,6 +13,7 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
 
 
 
