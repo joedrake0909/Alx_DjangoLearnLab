a03f51167f6db29c7038ead06953ae32f3ca74e5
Joseph_Bio <josephbiossey03@gmail.com>
2025-12-27 06:18:33 +0000

    Pip install

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -6,4 +7,4 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
-    author =  models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    author =  models.ForeignKey('User', on_delete=models.CASCADE)
